b058525d9a2fcf6cda3c6b8dc4b47bcc2e52e4e7
Oleg Akchurin <OAkchurin@IBS.ru>
2021-06-08 07:40:45 +0300

    update

--- constants/stripe-prices.ts
+++ constants/stripe-prices.ts
@@ -1,19 +1,19 @@
 export const STRIPE_PRICES: { [key: string]: string } = {
-  school_guitar_1: 'price_1HouDuHTf8fYJsx5Rc1RlU5y',
-  school_guitar_3: 'price_1HouFbHTf8fYJsx59pOaJBp7',
-  school_guitar_6: 'price_1HouGgHTf8fYJsx5SyGE1WpG',
+  school_guitar_1: 'price_1IzeXVHTf8fYJsx5dWO88a6Z',
+  school_guitar_3: 'price_1IzeWxHTf8fYJsx5V3BkXGUY',
+  school_guitar_6: 'price_1IzeUkHTf8fYJsx5SIfCrYeL',
 
-  school_keys_1: 'price_1HpAITHTf8fYJsx54kyeAYBs',
-  school_keys_3: 'price_1HpAHnHTf8fYJsx5X6CtiU4o',
-  school_keys_6: 'price_1HpAH0HTf8fYJsx57XXrFRy6',
+  school_keys_1: 'price_1IzepZHTf8fYJsx5vG1Vnitk',
+  school_keys_3: 'price_1IzeopHTf8fYJsx5vrFAlxwS',
+  school_keys_6: 'price_1IzenfHTf8fYJsx5WDNAuw2W',
 
-  school_sax_1: 'price_1HpANlHTf8fYJsx5wGFItLNl',
-  school_sax_3: 'price_1HpAN5HTf8fYJsx53tfblIU5',
-  school_sax_6: 'price_1HpAMPHTf8fYJsx5hynugpD8',
+  school_sax_1: 'price_1IzevkHTf8fYJsx55wnEQwUQ',
+  school_sax_3: 'price_1IzeuPHTf8fYJsx56dIm1Y0e',
+  school_sax_6: 'price_1IzetoHTf8fYJsx5aDaSohAs',
 
   college_guitar_1: 'price_1IzeZjHTf8fYJsx5qbkXV6aN',
-  college_guitar_6: 'price_1IzeYQHTf8fYJsx5PM2hFLkg',
-  college_guitar_12: 'price_1IzeZAHTf8fYJsx5XFQV1eiq',
+  college_guitar_6: 'price_1IzeZAHTf8fYJsx5XFQV1eiq',
+  college_guitar_12: 'price_1IzeYQHTf8fYJsx5PM2hFLkg',
 
   college_keys_1: 'price_1HpAL2HTf8fYJsx5xEpb2qD8',
   college_keys_6: 'price_1HpAK3HTf8fYJsx508GXG2mk',
--- ui/components/pricing/payment/payment.tsx
+++ ui/components/pricing/payment/payment.tsx
@@ -2,47 +2,70 @@ import * as React from 'react';
 import { inject, observer } from 'mobx-react';
 import block from 'bem-css-modules';
 import style from './payment.module.sass';
-import { Paragraph } from '../../../common';
+import { Button, Paragraph } from '../../../common';
 import { PayPalButton } from './paypal-button/paypal-button';
 import { RootStore } from '../../../../stores';
-import { SERVICE_NAME } from '../../../../constants';
+import { MODALS, SERVICE_NAME } from '../../../../constants';
 import { ucFirst } from '../../../../helpers';
 import { StripeButton } from './stripe-button/stripe-button';
 
 const b = block(style);
 
 type PaymentProps = {
+  isAuth: boolean,
   service_name: SERVICE_NAME,
-  instrument: string
+  instrument: string,
+  onShow: (id_window: MODALS) => void
 };
 type PaymentState = {};
 
 @inject((store: RootStore) => ({
+  isAuth: store.authStore.isAuth,
   service_name: store.systemStore.service_name,
-  instrument: store.pricingStore.selected_instrument
+  instrument: store.pricingStore.selected_instrument,
+  onShow: store.modalsStore.show
 }))
 @observer
 export class Payment extends React.Component<PaymentProps, PaymentState> {
 
   static defaultProps = {
+    isAuth: false,
     service_name: SERVICE_NAME.SCHOOL,
-    instrument: ''
+    instrument: '',
+    onShow: () => console.log('Not set handler')
+  };
+
+  handleOnShow = () => {
+    const { onShow } = this.props;
+    onShow(MODALS.SIGN_IN);
   };
 
   render() {
-    const { service_name, instrument } = this.props;
+    const { service_name, instrument, isAuth } = this.props;
 
     return (
       <div className={b(null)}>
         <div className='container'>
-          <div className='row mb-5'>
-            <div className='col-lg-6 d-flex justify-content-center align-items-start'>
-              <StripeButton />
-            </div>
-            <div className='col-lg-6'>
-              <PayPalButton />
-            </div>
-          </div>
+
+          {isAuth
+          ? (
+              <div className='row mb-5'>
+                <div className='col-lg-6 d-flex justify-content-center align-items-start'>
+                  <StripeButton />
+                </div>
+                <div className='col-lg-6'>
+                  <PayPalButton />
+                </div>
+              </div>
+            )
+          : (
+              <div className='row mb-5'>
+                <div className='col-lg-12 d-flex justify-content-center align-items-start'>
+                  <Button onClick={this.handleOnShow}>Sign in</Button>
+                </div>
+              </div>
+            )}
+
 
           <div className='row mb-4'>
             <div className='col-lg-12'>
