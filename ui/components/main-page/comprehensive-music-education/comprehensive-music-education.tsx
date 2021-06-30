3fdba97f0e35c4cb67716a154096a8fd1b601799
Oleg Akchurin <OAkchurin@IBS.ru>
2021-06-30 15:36:50 +0300

    update

--- ui/components/main-page/comprehensive-music-education/comprehensive-music-education.tsx
+++ ui/components/main-page/comprehensive-music-education/comprehensive-music-education.tsx
@@ -29,13 +29,13 @@ export class ComprehensiveMusicEducation extends React.Component<ComprehensiveMu
                  className={b('logotype')} />
           </picture>
 
-          <h1 className={b('title')}>Comprehensive music education</h1>
+          <h1 className={b('title')}>Learn to play music instruments 5 times faster</h1>
           <p className={b('description')}>
             <span style={{
-              fontWeight: 600,
+              fontWeight: 200,
               letterSpacing: 1.3,
               fontSize: '22px'
-            }}>{`Start your 14 days trial right now`.toUpperCase()}</span><br />
+            }}>{`with the advanced education system`}</span><br />
           </p>
 
           <div className={b('button')}>
