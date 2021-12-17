e26829ef2e1477123a74ea348266b0368a31277d
@oakchuri <oakchuri@megafon.ru>
2021-12-17 21:29:17 +0300

    - Update

--- pages/head-block.tsx
+++ pages/head-block.tsx
@@ -17,27 +17,27 @@ export default class HeadBlock extends React.Component<HeadBlockProps, HeadBlock
 
   getGTM = (): React.ReactNode => {
     return (
-      <script
-        dangerouslySetInnerHTML={{
-          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      <script defer
+              dangerouslySetInnerHTML={{
+                __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
       new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
       j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
       'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
     })(window,document,'script','dataLayer','GTM-PPT365K');`
-        }}
+              }}
       />
     );
   };
 
   getGTAG = (): React.ReactNode => {
     return (
-      <script
-        dangerouslySetInnerHTML={{
-          __html: `window.dataLayer = window.dataLayer || [];
+      <script defer
+              dangerouslySetInnerHTML={{
+                __html: `window.dataLayer = window.dataLayer || [];
       function gtag(){dataLayer.push(arguments);}
       gtag('js', new Date());
       `
-        }}
+              }}
       />
     );
   };
@@ -48,17 +48,15 @@ export default class HeadBlock extends React.Component<HeadBlockProps, HeadBlock
     return (
       <Head>
         <title>{title}</title>
-        <meta name="description" content={description} />
-        <meta name="keywords" content={keywords} />
-        <meta key={'viewport'} name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-        <link
-          rel="stylesheet"
-          href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css"
-        />
-        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
-        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
-        <meta name="yandex-verification" content="9cd91603377bfecd" />
-        <script src="https://widget.cloudpayments.ru/bundles/cloudpayments" />
+        <meta name='description' content={description} />
+        <meta name='keywords' content={keywords} />
+        <meta key={'viewport'} name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no' />
+        <link rel='stylesheet'
+              href='https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css' />
+        <link rel='shortcut icon' href='/favicon.ico' type='image/x-icon' />
+        <link rel='icon' href='/favicon.ico' type='image/x-icon' />
+        <meta name='yandex-verification' content='9cd91603377bfecd' />
+        <script defer src='https://widget.cloudpayments.ru/bundles/cloudpayments' />
         {this.getGTM()}
         {this.getGTAG()}
       </Head>
