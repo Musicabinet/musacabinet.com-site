353430cd0dbaf0f7d8a2e7a6905e4c72ecfb5abf
@oakchuri <oakchuri@megafon.ru>
2022-04-07 15:07:21 +0300

    - update

--- ui/common/pagination/pagination.tsx
+++ ui/common/pagination/pagination.tsx
@@ -3,6 +3,8 @@ import { inject, observer } from 'mobx-react';
 import block from 'bem-css-modules';
 import style from './pagination.module.sass';
 import { LessonStore, RootStore, SystemStore } from '../../../stores';
+import PaginationRC from 'rc-pagination';
+import { getIcon, LIST_ICON } from '../icons';
 
 const b = block(style);
 
@@ -22,10 +24,15 @@ export class Pagination extends React.Component<PaginationProps, PaginationState
   static defaultProps = {
     lessonStore: {},
     systemStore: {}
-  }
+  };
 
   handleOnClick = (score_number: number) => {
     const { lessonStore } = this.props;
+
+    if(score_number === -1 || score_number > lessonStore.scoresTotal){
+      return false;
+    }
+
     lessonStore.setCurrentScore(score_number);
   };
 
@@ -43,15 +50,41 @@ export class Pagination extends React.Component<PaginationProps, PaginationState
     return pagination;
   };
 
+  itemRender = (current: number, type: string, element: React.ReactNode): React.ReactNode => {
+
+    const newPage = current - 1;
+
+    if (type === 'prev') {
+      return <a data-page-prev={true}
+                onClick={() => this.handleOnClick(newPage)}>{getIcon(LIST_ICON.ARROW_LEFT, '')}</a>;
+    }
+    if (type === 'next') {
+      return <a data-page-next={true}
+                onClick={() => this.handleOnClick(newPage)}>{getIcon(LIST_ICON.ARROW_RIGHT, '')}</a>;
+    }
+
+    if (type === 'page') {
+      return <a data-page-link={true} onClick={() => this.handleOnClick(newPage)}>{current}</a>;
+    }
+
+    return element;
+  };
+
   render() {
     const { lessonStore, systemStore } = this.props;
+    const currentPage = lessonStore.currentScore + 1;
+
 
     return (
       <div className={b(null, {
         [systemStore.service_name]: true,
         hidden: lessonStore.scoresTotal < 2
       })}>
-        {this.generate()}
+        <PaginationRC current={currentPage}
+                      total={lessonStore.scoresTotal}
+                      itemRender={this.itemRender}
+                      pageSize={1}
+                      className={`${systemStore.service_name}`} />
       </div>
     );
   }
