fbeed6ba0e81f0c72d4ac59bfadee9a2e4265e9f
@oakchuri <oakchuri@megafon.ru>
2022-04-21 16:03:05 +0300

    - Long name lesson

--- stores/lesson.ts
+++ stores/lesson.ts
@@ -317,7 +317,7 @@ export class LessonStore implements LessonI {
 
   @computed
   get disabledSwitcher(): boolean {
-    return this.scores.length > 0 && this.scores[1].items.length === 0;
+    return this.scoresTotal < 1;
   }
 
   @computed
